2caf94278311072e7f02a1baab15bacdb8372276
Michael Berger <vo1kod4v@gmail.com>
2016-04-02 21:15:57 +0300

    project almost done

---
+++ run_analysis.R
@@ -0,0 +1,45 @@
+#rm(list = "data")
+library(dplyr)
+
+features <- read.table("UCI HAR Dataset/features.txt",header = FALSE,stringsAsFactors = FALSE)
+#features <- features["V2"]
+
+if (!exists("X_train_src")) # dont read these massive tables at every run
+{
+  X_train_src <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features[,2] ,header = FALSE,stringsAsFactors = FALSE)#,check.names=FALSE)
+  X_test_src <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features[,2] ,header = FALSE,stringsAsFactors = FALSE)#,check.names=FALSE)
+}
+
+features <-colnames(X_train_src)
+inds<- grep("mean|std",features)
+reqFeatures <- features[inds]
+inds2<- grep('^(?!.*meanFreq).*$',reqFeatures, perl =T)
+reqFeatures <- reqFeatures[inds2]
+
+
+X_train<- X_train_src[,reqFeatures]
+X_test<- X_test_src[,reqFeatures]
+
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt",header = FALSE,stringsAsFactors = FALSE, col.names = "actid")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt",header = FALSE,stringsAsFactors = FALSE, col.names = "actid")
+
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt",header = FALSE,stringsAsFactors = FALSE, col.names = "subject")
+subject_test<- read.table("UCI HAR Dataset/test/subject_test.txt",header = FALSE,stringsAsFactors = FALSE, col.names = "subject")
+
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt",header = FALSE,stringsAsFactors = FALSE, col.names = c("actid","activityName"))
+
+activities_train <- merge(y_train,activity_labels, by = "actid",  all = TRUE ,sort = FALSE)
+activities_test <- merge(y_test,activity_labels, by = "actid",  all = TRUE ,sort = FALSE)
+
+X_train <-cbind(subject_train["subject"],activities_train["activityName"], X_train)
+X_test <-cbind(subject_test["subject"], activities_test["activityName"], X_test)
+
+X<- full_join(X_train,X_test)
+
+Xg<- group_by(X, subject, activityName)
+Xmean <- summarise_each(Xg, funs(mean))
+
+write.table(Xmean, file = "Q5.txt", row.names = FALSE)
+
+
+
